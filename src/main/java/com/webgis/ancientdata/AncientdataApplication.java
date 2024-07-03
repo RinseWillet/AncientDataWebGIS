7069ead049ee923ea398bf9f9d093bf42c70b016
RinseWillet <r.willet@gmail.com>
2024-07-03 13:42:11 +0200

    initial package

---
+++ src/main/java/com/webgis/ancientdata/AncientdataApplication.java
@@ -0,0 +1,13 @@
+package com.webgis.ancientdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncientdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AncientdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webgis/ancientdata/AncientdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webgis.ancientdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AncientdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
