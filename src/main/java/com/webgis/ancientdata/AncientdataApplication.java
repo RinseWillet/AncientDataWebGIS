1ab413bf96611dc496cc4327c9aa0bd568e20dce
r-willet <rinse.willet@luminis.eu>
2026-06-02 11:47:33 +0200

    added google drive backup for images

--- src/main/java/com/webgis/ancientdata/AncientdataApplication.java
+++ src/main/java/com/webgis/ancientdata/AncientdataApplication.java
@@ -1,13 +1,19 @@
 package com.webgis.ancientdata;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AncientdataApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(AncientdataApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(AncientdataApplication.class, args);
-		System.out.println("yes, Server app running");
+		LOGGER.info("AncientDataWebGIS server is running");
 	}
 }
